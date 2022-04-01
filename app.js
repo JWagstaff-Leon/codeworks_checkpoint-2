9598c5df813f0281642308a5508db8edf2b163a3
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-01 16:28:52 -0600

    base functional version

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// TODO balance changes
+// TODO fix click frequency being slow
 // #region variables
 
 const player = 
@@ -5,7 +7,7 @@ const player =
     resource: 0,
     lifetimeResource: 0,
     clicks: 0,
-    rollingTotalClicksPerSecond: [] // TODO decide how many seconds this will count to round over
+    clicksRollingTotal: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 }
 
 /*******************************************************************************
@@ -38,13 +40,14 @@ const player =
 *******************************************************************************/
 const upgrades =
 {
-    clickAdders:
+    autoClickers:
     {
         pickaxe:
         {
             name: "Pickaxe",
-            costGrowAmount: 10,
-            costGrowFactor: 0,
+            shopname: "⛏️",
+            growBase: 10,
+            growFactor: 0,
             owned: 0,
             costConstant: 10,
             priceFunction: linear,
@@ -57,8 +60,9 @@ const upgrades =
         worker:
         {
             name: "Hire Worker",
-            costGrowAmount: 10,
-            costGrowFactor: 1,
+            shopname: "👷",
+            growBase: 10,
+            growFactor: 1,
             owned: 0,
             costConstant: 10,
             priceFunction: exponential,
@@ -66,13 +70,14 @@ const upgrades =
         }
     },
 
-    autoAdders:
+    autoGainers:
     {
         fabrication:
         {
             name: "Research Fabrication",
-            costGrowAmount: 10,
-            costGrowFactor: 1,
+            shopname: "⚛️",
+            growBase: 10,
+            growFactor: 1,
             owned: 0,
             costConstant: 10,
             priceFunction: exponential,
@@ -82,11 +87,12 @@ const upgrades =
 
     globalMultipliers:
     {
-        thievry:
+        thievery:
         {
             name: "Improptu Cooperation",
-            costGrowAmount: 10,
-            costGrowFactor: 0,
+            shopname: "🤝",
+            growBase: 10,
+            growFactor: 0,
             owned: 0,
             costConstant: 10,
             priceFunction: linear,
@@ -95,85 +101,288 @@ const upgrades =
     }
 }
 
-let intervalIds = 
-{
-    autoSpeed: undefined
-}
-
 let elements = 
 {
+    currentResource: document.getElementById("current-resource"),
+    lifetimeResource: document.getElementById("lifetime-resource"),
+    totalGps: document.getElementById("total-gps"),
+    manualCps: document.getElementById("manual-cps"),
+    autoCps: document.getElementById("auto-cps"),
+    autoGps: document.getElementById("auto-gps"),
+    clickMultiplier: document.getElementById("click-multiplier"),
+    globalMultiplier: document.getElementById("global-multiplier"),
     inventory: document.getElementById("inventory"),
-    shop: document.getElementById("shop"),
+    boughtUpgrades: document.getElementById("bought-upgrades"),
+    autoGainers: document.getElementById("auto-gainers"),
+    autoClickers: document.getElementById("auto-clickers"),
+    clickMultipliers: document.getElementById("click-multipliers"),
+    globalMultipliers: document.getElementById("global-multipliers"),
     stats: document.getElementById("stats")
 }
 
 // #endregion
-// #region game functions
+// #region click functions
 
 function clickTarget()
 {
-    player.resource += 1;
+    player.resource += calculateclickMultiplier();
+    player.lifetimeResource += calculateclickMultiplier();
+    player.clicks += 1;
 
     drawInventory();
     drawStats();
+    drawShop();
 }
 
-function updateAutoSpeed()
+function updateManualClicks()
 {
-    clearInterval(intervalIds.clicksPerSecond)
+    player.clicksRollingTotal.shift();
+    player.clicksRollingTotal.push(player.clicks);
+    player.clicks = 0;
+    drawStats();
+}
 
-    startInterval(autoClick, 1000) // TODO determine this time
+function autoClick()
+{
+    player.resource += calculateAutoAmount();
+    player.lifetimeResource += calculateAutoAmount();
+
+    drawInventory();
+    drawStats();
+    drawShop();
+}
+
+// #endregion
+// #region calculation functions
+
+function calculateManualCps()
+{
+    let amount = 0;
+
+    for(let i = 0; i < player.clicksRollingTotal.length; i++)
+    {
+        amount += player.clicksRollingTotal[i];
+    }
+
+    amount /= player.clicksRollingTotal.length;
+    return amount;
 }
 
 function calculateAutoAmount()
 {
+    let amount = 0;
 
+    amount += calculateAutoClicks();
+    amount *= calculateclickMultiplier();
+
+    amount += calculateAutoGains();
+    amount *= calculateGlobalMultiplier();
+
+    return amount;
 }
+
+function calculateAutoClicks()
+{
+    let amount = 0;
+
+    for(let key in upgrades.autoClickers)
+    {
+        amount += upgrades.autoClickers[key].owned * upgrades.autoClickers[key].efficacy;
+    }
+
+    return amount;
+}
+
+function calculateclickMultiplier()
+{
+    let amount = 1;
+
+    for(let key in upgrades.clickMultipliers)
+    {
+        amount += upgrades.clickMultipliers[key].owned * upgrades.clickMultipliers[key].efficacy;
+    }
+
+    return amount;
+}
+
+function calculateAutoGains()
+{
+    let amount = 0;
+
+    for(let key in upgrades.autoGainers)
+    {
+        amount += upgrades.autoGainers[key].owned * upgrades.autoGainers[key].efficacy;
+    }
+
+    return amount;
+}
+
+function calculateGlobalMultiplier()
+{
+    let amount = 1;
+
+    for(let key in upgrades.globalMultipliers)
+    {
+        amount += upgrades.globalMultipliers[key].owned * upgrades.globalMultipliers[key].efficacy;
+    }
+
+    return amount;
+}
+
+// #endregion
+// #region shop functions
 
 function purchaseUpgrade(category, upgrade)
 {
-    if(upgrades[category][upgrade].increaseFunction(baseCost, owned) < player.resource)
+    const item = upgrades[category][upgrade];
+    if(getItemCost(item) < player.resource)
     {
-        playerResource -= upgrades[category][upgrade].increaseFunction(baseCost, owned);
-        upgrades[category][upgrade].owned += 1;
+        player.resource -= getItemCost(item);
+        item.owned += 1;
     }
 
-    updateAutoSpeed();
-
     drawInventory();
-    drawResource();
+    drawShop();
+    drawStats();
+}
 
+function getItemCost(item)
+{
+    return item.priceFunction(item.growBase, item.growFactor, item.owned, item.costConstant);
+}
+
+function linear(growBase, growFactor, owned, costConstant)
+{
+    return growBase * (1 + (growFactor * owned)) + costConstant;
+}
+
+function exponential(growBase, growFactor, owned, costConstant)
+{
+    return growBase * (growFactor ** (owned - 1)) + costConstant;
 }
 
 // #endregion
-// #region priceFunctions
-
-function linear(growBase, costFactor, owned, costConstant)
-{
-    return growBase * (1 + (costFactor * owned)) + costConstant;
-}
-
-function exponential(growBase, costFactor, owned, costConstant)
-{
-    return growBase * (costFactor ** (owned - 1)) + costConstant;
-}
-
-// #endregion
-//  #region draw functions
-
-function drawResource()
-{
-
-}
+// #region draw functions
 
 function drawInventory()
 {
+    elements.lifetimeResource.innerText = Math.floor(player.lifetimeResource.toString());
+    
+    elements.currentResource.innerText = Math.floor(player.resource.toString());
 
+    let template = "";
+
+    for(let key1 in upgrades)
+    {
+        for(let key2 in upgrades[key1])
+        {
+            if(upgrades[key1][key2].owned > 0)
+            {
+                template +=
+                `
+                <p class="my-2 text-warning">${upgrades[key1][key2].name}: x${Math.floor(upgrades[key1][key2].owned)}</p>
+                `;
+            }
+        }
+    }
+
+    elements.boughtUpgrades.innerHTML = template;
 }
 
 function drawStats()
 {
+    elements.totalGps.innerText = Math.floor(calculateAutoAmount() + calculateManualCps()).toString();
+    elements.manualCps.innerText = calculateManualCps().toString();
+    elements.autoCps.innerText = calculateAutoClicks().toString();
+    elements.autoGps.innerText = calculateAutoGains().toString();
+    elements.clickMultiplier.innerText = calculateclickMultiplier().toString();
+    elements.globalMultiplier.innerText = calculateGlobalMultiplier().toString();
+}
+
+function drawShop()
+{
+    let template = "";
+
+    for(let key in upgrades.autoGainers)
+    {
+        const item = upgrades.autoGainers[key];
+        template +=
+        `
+        <button class="my-1 d-flex btn btn-warning" onclick="purchaseUpgrade('autoGainers', '${key}')" title="${item.name}">
+            <hr>
+            <div class="d-flex flex-column text-end me-1">
+                <span class="text-light ms-1">+${item.efficacy}/s</span>
+                <span class="text-light ms-1">${getItemCost(item)} Cheese</span>
+            </div>
+            <span class="my-auto">${item.shopname}</span>
+        </button>
+        `;
+    }
     
+    elements.autoGainers.innerHTML = template;
+
+    template = "";
+
+    for(let key in upgrades.autoClickers)
+    {
+        const item = upgrades.autoClickers[key];
+        template +=
+        `
+        <button class="my-1 d-flex btn btn-warning" onclick="purchaseUpgrade('autoClickers', '${key}')" title="${item.name}">
+            <hr>
+            <div class="d-flex flex-column text-end me-1">
+                <span class="text-light ms-1">+${item.efficacy}/s</span>
+                <span class="text-light ms-1">${getItemCost(item)} Cheese</span>
+            </div>
+            <span class="my-auto">${item.shopname}</span>
+        </button>
+        `;
+    }
+
+    elements.autoClickers.innerHTML = template;
+    
+    template = "";
+
+    for(let key in upgrades.clickMultipliers)
+    {
+        const item = upgrades.clickMultipliers[key];
+        template +=
+        `
+        <button class="my-1 d-flex btn btn-warning" onclick="purchaseUpgrade('clickMultipliers', '${key}')" title="${item.name}">
+            <hr>
+            <div class="d-flex flex-column text-end me-1">
+                <span class="text-light ms-1">+${item.efficacy}/s</span>
+                <span class="text-light ms-1">${getItemCost(item)} Cheese</span>
+            </div>
+            <span class="my-auto">${item.shopname}</span>
+        </button>
+        `;
+    }
+
+    elements.clickMultipliers.innerHTML = template;
+
+    template = "";
+
+    for(let key in upgrades.globalMultipliers)
+    {
+        const item = upgrades.globalMultipliers[key];
+        template +=
+        `
+        <button class="my-1 d-flex btn btn-warning" onclick="purchaseUpgrade('globalMultipliers', '${key}')" title="${item.name}">
+            <hr>
+            <div class="d-flex flex-column text-end me-1">
+                <span class="text-light ms-1">+${item.efficacy}/s</span>
+                <span class="text-light ms-1">${getItemCost(item)} Cheese</span>
+            </div>
+            <span class="my-auto">${item.shopname}</span>
+        </button>
+        `;
+    }
+
+    elements.globalMultipliers.innerHTML = template;
 }
 
 // #endregion
+
+setInterval(updateManualClicks, 1000);
+setInterval(autoClick, 1000);
