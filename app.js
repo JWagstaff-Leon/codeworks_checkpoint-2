3ff1777b2d915f96d8d5f59c57ea4a446e63b1d7
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-01 13:27:27 -0600

    groundwork to start implementing gameplay

--- app.js
+++ app.js
@@ -1,4 +1,58 @@
-// TODO add inventory
-// TODO add upgrades object
-// TODO add interval functions
-// TODO add intervals object
+// #region variables
+
+const player = 
+{
+    resource: 0,
+    lifetimeResource: 0,
+    clicks: 0,
+    rollingTotalClicksPerSecond: [] // TODO decide how many seconds this will count to round over
+}
+
+// TODO flesh out upgrades object
+const upgrades =
+{
+    clickMultipliers:
+    {
+        upgradeTemplate:
+        {
+            name: "Template_Click_Upgrade",
+            baseCost: 1,
+            owned: 0,
+            increaseFunction: undefined
+        }
+    },
+
+}
+
+let intervalIds = 
+{
+    clicksPerSecond: undefined
+}
+
+let elements = 
+{
+    inventory: document.getElementById("inventory"),
+    shop: document.getElementById("shop"),
+    stats: document.getElementById("stats")
+}
+
+// #endregion
+// #region game functions
+
+function click()
+{
+    player.resource += 1;
+    console.log(player.resource);
+}
+
+// #endregion
+//  #region draw functions
+
+function drawInventory()
+{
+    let template = "";
+
+    elements.stats.innerHTML = template;
+}
+
+// #endregion
